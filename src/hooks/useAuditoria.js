2aa3a85d349f0e3bbeff2108f848f88a606c6b46
João Victor Ferreira <jferrcon@gmail.com>
2026-09-04 10:17:59 -0300

    Delete src directory

--- src/hooks/useAuditoria.js
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback, useEffect } from "react";
-import { assinarAuditoria } from "../data/auditoriaStore.js";
-
-export function useAuditoria() {
-  const [, forcar] = useState(0);
-  const rerenderizar = useCallback(() => forcar(n => n + 1), []);
-  useEffect(() => assinarAuditoria(rerenderizar), [rerenderizar]);
-}
